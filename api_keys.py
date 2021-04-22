6a4b1be111e4bb49cd1e5bb2069c077d785d92c4
Abhinav Shrestha <79868048+abhikenobi@users.noreply.github.com>
2021-04-22 05:37:37 -0600

    finished retrieving school data
    retrieved name and rank of any high, middle, and elementary school in a 5mile radius of each house

--- api_keys.py
+++ api_keys.py
@@ -7,9 +7,7 @@ g_key = "AIzaSyAVuJ6ZseQNx1zpyYbfJIzbcekWvN4DutU"
 # Walk Score API key
 walk_key = "YOUR KEY HERE!"
 
-# School Score API key
-school_key = "YOUR KEY HERE!"
-
 # School Digger API key
 digger_key = "ea14676381f70e7dccb7e08aa287528c"
+digger_id = "52621e3a"
 geo_key = "AIzaSyCQfDhUnIzas_GAcTWd10OOsLH2G4DE9-Q"
