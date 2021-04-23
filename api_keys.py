70b828adf586493fb4fcf77652e668fc225b2c6d
Abhinav Shrestha <79868048+abhikenobi@users.noreply.github.com>
2021-04-23 10:45:33 -0600

    adding to script

--- api_keys.py
+++ api_keys.py
@@ -5,9 +5,9 @@ weather_api_key = "6f153332627c4540983d5e0900d176bd"
 g_key = "AIzaSyAVuJ6ZseQNx1zpyYbfJIzbcekWvN4DutU"
 
 # Walk Score API key
-walk_key = "YOUR KEY HERE!"
+walk_key = "dbbd3eb78dabd9022d630f7fb8edc502"
 
-# School Score API key
-school_key = "YOUR KEY HERE!"
+# School Digger API key
+digger_key = "ea14676381f70e7dccb7e08aa287528c"
+digger_id = "52621e3a"
 
-#
